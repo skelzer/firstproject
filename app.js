b688a06be123384e521ec2e9565dba83fa31c083
Miguel <miguel.luque@dacadoo.com>
2022-06-05 16:59:35 +0200

    Routing to different html files

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
+
 function callback(request,response){
-    fs.readFile("index.html", function (err,data){
+    const base = url.parse(request.url);
+    const pathname = base.pathname;
+    const fileName = '.'+ pathname+'.html';
+    fs.readFile(fileName, function (err,data){
+        if(err){
+            response.writeHead(404,{'Content-Type':'text/html'});
+            return response.end('<h1>Not Found</h1>')
+        }
         response.writeHead(200, {'Content-Type':'text/html'});
         response.write(data);
         response.end();
-    });
+    })
+
+
+
 }
 
 http.createServer(callback).listen(8080);
