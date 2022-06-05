c0d6bc728077236c7d9ed7b8b49a3b50955b5181
Miguel <miguel.luque@dacadoo.com>
2022-06-05 17:36:53 +0200

    Added locales for strings

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 const http = require("http")
 const fs = require("fs")
 const url = require("url")
+const parser = require("accept-language-parser")
+const {parse} = require("accept-language-parser");
+
+function retrieveString(key, locale){
+    var string = JSON.parse(fs.readFileSync('./strings.json','utf-8'))
+    if (string){
+        return string[key][locale]
+    }
+    else{
+        return false
+    }
+
+}
+
 
 function callback(request,response){
     const base = url.parse(request.url);
     const pathname = base.pathname;
     const fileName = '.'+ pathname+'.html';
-    fs.readFile(fileName, function (err,data){
+    var userLocale= parser.parse(request.headers["accept-language"]);
+    userLocale = userLocale[0]["code"]+"-"+userLocale[0]["region"]
+        fs.readFile(fileName, function (err,data){
         if(err){
             response.writeHead(404,{'Content-Type':'text/html'});
             return response.end('<h1>Not Found</h1>')
         }
         response.writeHead(200, {'Content-Type':'text/html'});
         response.write(data);
+        response.write("\n"+retrieveString("message", userLocale))
         response.end();
     })
-
-
-
 }
 
 http.createServer(callback).listen(8080);
