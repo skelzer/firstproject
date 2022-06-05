a21e83471d3c58d3db664dc5d37cc20582f7eed0
Miguel <miguel.luque@dacadoo.com>
2022-06-05 14:35:34 +0200

    Modules

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hey there man")
+var http = require("http")
+var output =require("./module.js")
+function callback(request,response){
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.write("Heyo man\n")
+    response.end("Hello World")
+    console.log(output.greeting)
+}
+
+http.createServer(callback).listen(8080);
