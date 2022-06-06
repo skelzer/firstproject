d7ac08795b32ea90a8800b6cd26ead95cd29b1a2
Miguel Luque <miguel.luque@dacadoo.com>
2022-06-06 22:43:03 +0200

    Using Express

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express()
 const port = 7070
 
 
+app.use(express.static(__dirname))
+
+const users = [
+    {user:"Migue", password:"pass1"},
+    {user:"Migue2", password:"pass2"},
+    {user:"Migue3", password:"pass3 "}
+]
 
 
 function retrieveString(key, locale){
@@ -24,12 +31,20 @@ function retrieveString(key, locale){
 function callback(request,response){
     var userLocale= parser.parse(request.headers["accept-language"]);
     userLocale = userLocale[0]["code"]+"-"+userLocale[0]["region"]
-    response.send("Hey there")
+    response.send("Hey theddre")
     response.end();
     }
 
 
 
-app.get("/", callback)
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
+app.get("/users", function (req, res){
+    res.send(users)
+})
+
+
+
 app.listen(port, function (){return console.log("Server started");})
 //http.createServer(callback).listen(8080);
