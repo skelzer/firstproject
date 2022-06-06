f1b3894b37dc7e40f056489736e8061931ac0563
Miguel Luque <miguel.luque@dacadoo.com>
2022-06-06 14:49:07 +0200

    Using Express

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const http = require("http")
 const fs = require("fs")
 const url = require("url")
 const parser = require("accept-language-parser")
+const express = require('express')
+const app = express()
+const port = 7070
+
+
+
 
 function retrieveString(key, locale){
     var string = JSON.parse(fs.readFileSync('./strings.json','utf-8'))
@@ -16,21 +22,14 @@ function retrieveString(key, locale){
 
 
 function callback(request,response){
-    const base = url.parse(request.url);
-    const pathname = base.pathname;
-    const fileName = '.'+ pathname+'.html';
     var userLocale= parser.parse(request.headers["accept-language"]);
     userLocale = userLocale[0]["code"]+"-"+userLocale[0]["region"]
-        fs.readFile(fileName, function (err,data){
-        if(err){
-            response.writeHead(404,{'Content-Type':'text/html'});
-            return response.end('<h1>Not Found</h1>')
-        }
-        response.writeHead(200, {'Content-Type':'text/html'});
-        response.write(data);
-        response.write("\n"+retrieveString("message", userLocale))
-        response.end();
-    })
-}
+    response.send("Hey there")
+    response.end();
+    }
 
-http.createServer(callback).listen(8080);
+
+
+app.get("/", callback)
+app.listen(port, function (){return console.log("Server started");})
+//http.createServer(callback).listen(8080);
